9dc0f7a6621d8c2b40fb51f8fe513ad6beaa5951
Om-19 <patilom1954@gmail.com>
2024-11-21 15:21:46 +0530

    Landing Page Created

---
+++ js/script.js
@@ -0,0 +1,49 @@
+import check from '../assets/check.svg'
+import star from '../assets/star.svg'
+
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+// init AOS animation
+AOS.init({
+    duration: 1000,
+    offset: 100,
+});
+
+const trendingSushis = [
+    'L.K.G., U.K.G. and II',
+    'Classes: III-V',
+    'Classes: VI-XII',
+];
+
+const trendingDrinks = [
+    "Science",
+    "Commerce",
+    "Arts",
+]
+
+const cards = [
+    {
+        imgSrc: computer-lab,
+        alt: "computer-lab",
+        title: "Computer Lab",
+        rating: "4.8",
+        price: "-"
+    },
+    {
+        imgSrc: library,
+        alt: "library",
+        title: "Library",
+        rating: "4.8",
+        price: "-",
+        active: true
+    },
+    {
+        imgSrc: science-lab,
+        alt: "science-lab",
+        title: "Science Lab",
+        rating: "4.8",
+        price: "-"
+    }
+];
